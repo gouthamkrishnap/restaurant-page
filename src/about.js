eb474f6de1aa3a105457a19c077d47f499a222d2
Goutham Krishna <224089015+gouthamkrishnap@users.noreply.github.com>
2026-08-14 22:43:34 +0530

    feat: implement basic about page

---
+++ src/about.js
@@ -0,0 +1,18 @@
+function aboutTab() {
+    const content = document.querySelector("#content");
+
+    const heading = document.createElement("h1");
+    heading.textContent = "ABOUT US";
+
+    const paragraph = document.createElement("p");
+    paragraph.textContent ="The Batcave Bistro is a Gotham-inspired restaurant serving delicious food after dark.";
+
+    const paragraph2 = document.createElement("p");
+    paragraph2.textContent ="Founded in the heart of Gotham, our restaurant combines great food, a mysterious atmosphere, and a passion for bringing people together.";
+
+    content.appendChild(heading);
+    content.appendChild(paragraph);
+    content.appendChild(paragraph2);
+}
+
+export default aboutTab;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import "./styles.css";
 
 import homeTab from "./home.js";
 import menuTab from "./menu.js";
+import aboutTab from "./about.js";
 
 console.log("works");
 homeTab();
