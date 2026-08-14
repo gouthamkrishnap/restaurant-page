9bfb9e84ae192de0389150fefc6549222c5ce319
Goutham Krishna <224089015+gouthamkrishnap@users.noreply.github.com>
2026-08-14 22:28:23 +0530

    feat: implement basic home page

---
+++ src/home.js
@@ -0,0 +1,21 @@
+import restaurantImage from "./images/The_Batcave_Bistro.png"
+
+function homeTab() {
+    const content = document.querySelector("#content");
+
+    const heading = document.createElement("h1");
+    heading.textContent = "Dine in the Shadows";
+
+    const image = document.createElement("img");
+    image.src = restaurantImage;
+    image.alt = "The Batcave Bistro";
+
+    const paragraph = document.createElement("p");
+    paragraph.textContent = "Welcome to The Batcave Bistro, where Gotham’s finest flavors come out after dark. Inspired by the legendary Dark Knight, we serve bold, unforgettable dishes in a cozy hideaway worthy of Bruce Wayne himself. Step inside, grab a seat, and discover a menu that even Batman would make time for.";
+
+    content.appendChild(heading);
+    content.appendChild(image);
+    content.appendChild(paragraph);
+}
+
+export default homeTab;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 import "./styles.css";
+import homeTab from "./home.js";
 
 console.log("works");
+homeTab();
